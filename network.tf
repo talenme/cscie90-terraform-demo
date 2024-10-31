1709aa9c70fe6fb8d3b46c2fe400c02dc5da5608
talenme <github@pyrian.com>
2024-10-30 21:30:22 -0400

    Update network.tf
    Revised security group configuration based on latest Terraform version changes. 
    
    Added Route Table, IGW

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-#Create VPC in us-east-1
+#Create VPC
 resource "aws_vpc" "vpc_master" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -12,7 +12,7 @@ data "aws_availability_zones" "azs" {
   state = "available"
 }
 
-#Create subnet # 1 in us-east-1
+#Create public subnet 
 resource "aws_subnet" "subnet" {
   availability_zone = element(data.aws_availability_zones.azs.names, 0)
   vpc_id            = aws_vpc.vpc_master.id
@@ -23,26 +23,60 @@ resource "aws_subnet" "subnet" {
   }
 }
 
+#Create an IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_master.id
+
+  tags = {
+    Name = "${terraform.workspace}-igw"
+  }
+}
+
+#Create a route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc_master.id
+
+  tags = {
+    Name = "${terraform.workspace}-route-table"
+  }
+}
+#Build a route to the internet
+resource "aws_route" "public_internet_gateway" {
+  route_table_id = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+#Associate our public_subnet with the route to the IGW
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
 
 #Create SG for allowing TCP/22 from anywhere, THIS IS FOR TESTING ONLY
 resource "aws_security_group" "sg" {
   name        = "${terraform.workspace}-sg"
   description = "Allow TCP/22"
   vpc_id      = aws_vpc.vpc_master.id
-  ingress {
-    description = "Allow 22 from our public IP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = {
     Name = "${terraform.workspace}-securitygroup"
   }
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv4" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4       = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
